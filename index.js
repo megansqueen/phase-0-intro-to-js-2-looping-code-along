3438cac61354c86871bdf18cf40e5c30c025f56a
Megan <esler6724@gmail.com>
2022-10-19 20:23:05 -0700

    commit tree 453d5c57ff67c97dd62c8c3f5ddb550aad2fd764

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const event = `surprise`
+
+function writeCards(names, event) {
+    const thankYouCards = [names.length]
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+
+    return thankYouCards;
+
+}
+
+function countDown(number) {
+    let countDown = number;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+countDown(4) 
